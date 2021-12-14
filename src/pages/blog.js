db07f92bf5a621a696ece5e34571526001562fc9
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-12-14 11:32:09 +0900

    ページに画像を追加

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,21 +1,35 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import matter from "gray-matter"
 
 const Blog = (props) => {
   console.log(props)
   return (
-    <div>
-      <h1>ブログページ</h1>
-      {props.blogs.map((blog, index) =>
-        <div key={index}>
-          <h3>{blog.frontmatter.title}</h3>
-          <p>{blog.frontmatter.date}</p>
-          <Link href={`/blog/${blog.slug}`}>
-            <a>Read More</a>
-          </Link>
+    <>
+      <div>
+        <div>
+          <h1>Blog</h1>
+          <p>エンジニアの日常生活をお届けします</p>
+          {props.blogs.map((blog, index) =>
+            <div key={index}>
+              <div>
+                <h3>{blog.frontmatter.title}</h3>
+                <p>{blog.frontmatter.excerpt}</p>
+                <p>{blog.frontmatter.date}</p>
+                <Link href={`/blog/${blog.slug}`}>
+                  <a>Read More</a>
+                </Link>
+              </div>
+              <div>
+                <Image src={blog.frontmatter.image} alt="card-image"
+                  height={300} width={1000} quality={90}
+                />
+              </div>
+            </div>
+          )}
         </div>
-      )}
-    </div>
+      </div>
+    </>
   )
 }
 
@@ -27,7 +41,7 @@ export async function getStaticProps() {
     const values = keys.map(context)
 
     const data = keys.map((key, index) => {
-      let slug = key.replace(/^.*[\\\/]/,'').slice(0, -3)
+      let slug = key.replace(/^.*[\\\/]/, '').slice(0, -3)
       const value = values[index]
       const document = matter(value.default)
       return {
